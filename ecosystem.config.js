ab963cebeb6ed28c8a8784aa7b29d4d9baa7df00
Vũ Huỳnh Minh <36500047+vhm205@users.noreply.github.com>
2024-06-15 22:23:21 +0700

    chore: update scripts package

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,14 +3,11 @@ module.exports = {
     {
       name: 'mind-reminder',
       script: 'dist/main.js',
-      instances: 6,
-      exec_mode: 'cluster',
+      // instances: 6,
+      // exec_mode: 'cluster',
       env: {
         PORT: 2005,
-        NODE_ENV: 'development',
-        REDIS_URL: 'redis://127.0.0.1:6379',
         API_VERSION: 1,
-        DOMAIN: 'http://localhost:2005',
       },
     },
   ],
